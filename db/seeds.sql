8c9e84102d14b3ddbfd8bef5ff8bc26a0b6feb85
Alex <alexmore080@gmail.com>
2022-10-04 23:28:46 -0500

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,13 @@
-INSERT INTO department (employee_name)
-VALUES ("Alex"),
-("Joe"),
-("Nydia");
+INSERT INTO department (dept_name)
+VALUES ("Engineering"),
+("Accounting"),
+("Customer Service");
+
+INSERT INTO roles (title,salary,department_id)
+VALUES ('Engineer','200000',1),
+("Finance Advisor","1000000",2);
+
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ('Nydia','Duron',1,0),
+('Joe','Kope',2,1),
+('Alex','Moreno',1,0);
