cbefec887a1c576d85c8afcbc86150831cd62cf5
Alex <alexmore080@gmail.com>
2022-10-04 23:28:38 -0500

    primary key/foreign key reference fixes

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,26 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    employee_name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(20) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    FOREIGN KEY (id) REFERENCES roles(id),
-    manager_id INT REFERENCES employee(id)
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
 )
